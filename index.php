1dafc000f0962472c123f85d8376a6605318125a
andregg2 <andregg@stud.ntnu.no>
2019-02-07 23:14:03 +0100

    Flyttet CSS mappen og enablet twig render for index.php

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 /*
 Twig renderer for index.html
 */
+require_once 'vendor/autoload.php';
 
-/*
-require_once '../twig/vendor/autoload.php';
+$emptyArray = array();
 
 $loader = new Twig_Loader_Filesystem('./templates');
 $twig = new Twig_Environment($loader, array(
     //'cache' => './compilation_cache', // Only enable cache when everything works correctly 
 ));
 
-echo $twig->render('index.html', NULL); //Replace NULL with data you want to pass
-*/
+echo $twig->render('index.html', $emptyArray); //Replace NULL with data you want to pass
