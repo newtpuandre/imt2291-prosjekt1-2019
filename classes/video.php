89784af98547b3d0208dd843c17eb9c52e1d231f
elisawaa@stud.ntnu.no <elisawbye@gmail.com>
2019-02-11 08:23:29 +0100

    Fylte inn litt grunnleggende informasjon i video klassen

--- classes/video.php
+++ classes/video.php
@@ -1,21 +1,33 @@
 <?php
-/*
-Dummy file for the video class
-*/
-
-/*
 
 class Video
 {
-    function __construct() {
+
+    public $videoid;
+    public $title;
+    public $description;
+    public $topic;
+    public $course;
+    
+
+
+
+
+    function __construct($videoid, $title, $description, $topic, $course) {
+        $this->videoid = $videoid;
+        $this->title = $title;
+        $this->description = $description;
+        $this->topic = $topic;
+        $this->course = $course;
+
         
     }
 
-    function __destruct() {
+    /*function __destruct() {
 
     }
+    */
 }
 
-*/
 
 ?>
