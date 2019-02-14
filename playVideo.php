fd828782ae125731efe7946688d547adaa9f1c59
andregg2 <andregg@stud.ntnu.no>
2019-02-14 13:53:44 +0100

    Utkast for playVideo.php/html lagt til. WIP

--- playVideo.php
+++ playVideo.php
@@ -1,1 +1,32 @@
 <?php
+
+require_once 'vendor/autoload.php';
+require_once 'classes/user.php';
+
+$content = array();
+
+session_start();
+
+if (isset($_SESSION['user'])) { //User is logged in
+
+    $user = new User($_SESSION['user']);
+
+    $content['userinfo'] = $user->returnEmail();
+    $content['userprivileges'] = $user->getPrivileges();
+    
+}
+
+if (isset($_GET['id'])) {
+    
+
+    
+} else { //Need a video id inorder for the site to have any purpose..
+    header("Location: index.php");
+}
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, array(
+    //'cache' => './compilation_cache', // Only enable cache when everything works correctly 
+));
+
+echo $twig->render('playVideo.html', $content);
