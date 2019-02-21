0c1b492b516b615b8e451fd26f1e5f2e0934bb99
andregg2 <andregg@stud.ntnu.no>
2019-02-21 15:20:10 +0100

    Lagd en enkel logo... Påbegynt oversikt over spillelister som er tilgjengelig.

--- playlist.php
+++ playlist.php
@@ -1,1 +1,23 @@
 <?php
+require_once 'vendor/autoload.php';
+require_once 'classes/user.php';
+
+$content = array();
+
+session_start();
+
+if (isset($_SESSION['user'])) { //User is logged in
+
+    $user = new User($_SESSION['user']);
+
+    $content['userinfo'] = $user->returnEmail();
+    $content['userprivileges'] = $user->getPrivileges();
+    
+}
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, array(
+    //'cache' => './compilation_cache', // Only enable cache when everything works correctly 
+));
+
+echo $twig->render('playlist.html', $content);
