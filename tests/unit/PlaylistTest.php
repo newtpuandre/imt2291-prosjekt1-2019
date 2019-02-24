f4648157b78a86a79a92553c16ac1b401b9594ee
andregg2 <andregg@stud.ntnu.no>
2019-02-24 14:32:12 +0100

    Mal for playlist class unit tests

--- tests/unit/PlaylistTest.php
+++ tests/unit/PlaylistTest.php
@@ -16,8 +16,74 @@ class PlaylistTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testSomeFeature()
+    public function testResolveVideos()
     {
 
     }
+
+    public function testAddVideoToPlaylist()
+    {
+
+    }
+
+    public function testDeletePlaylist()
+    {
+
+    }
+
+    public function testDeleteVideoFromPlaylist()
+    {
+
+    }
+
+    public function testEditPosition()
+    {
+
+    }
+
+    public function testInsertPlaylist()
+    {
+
+    }
+
+    public function testReturnPlaylists()
+    {
+
+    }
+
+    public function testReturnVideos()
+    {
+
+    }
+
+    public function testReturnPlaylistVideos()
+    {
+
+    }
+
+    public function testUpdatePlaylist()
+    {
+
+    }
+
+    public function testReturnAllPlaylists() 
+    {
+
+    }
+
+    public function testSubscribeToPlaylist() 
+    {
+
+    }
+
+    public function testUnsubscribeToPlaylist() 
+    {
+
+    }
+
+    public function testGetSubscribedPlaylists() 
+    {
+
+    }
+
 }
