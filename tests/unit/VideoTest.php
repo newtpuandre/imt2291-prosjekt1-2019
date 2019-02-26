ff21088acce69a022845efeaaac47be120f2a182
elisawaa@stud.ntnu.no <elisawbye@gmail.com>
2019-02-26 19:59:26 +0100

    en fungerende test.

--- tests/unit/VideoTest.php
+++ tests/unit/VideoTest.php
@@ -2,9 +2,31 @@
 
 require_once './classes/db.php';
 require_once './classes/video.php';
+require_once './classes/user.php';
 
 class VideoTest extends \Codeception\Test\Unit
 {
+     //Test related variables
+
+    //User
+    private $userid = "1";
+    private $name = "name namesen";
+    private $email = "test@test.test";
+    private $password = "something";
+    private $privileges = "2";
+    private $isTeacher = true;
+
+    //Video    
+    private $videoid = "1";
+    private $title = "testvideo";
+    private $desc = "desc";
+    private $topic = "topic";
+    private $course = "course";
+    private $thumbnail ="https://via.placeholder.com/150";
+    private $video = "testVideo";
+
+    private $db = null;
+
     /**
      * @var \UnitTester
      */
@@ -44,8 +66,16 @@ class VideoTest extends \Codeception\Test\Unit
     }
 
     public function testGetVideo()
-    {
-        
+    {        
+        $this->tester->haveInDatabase('users',['id' => $this->userid, 'name' => $this->name, 'email' => $this->email, 'password' => $this->password]);
+        $this->tester->haveInDatabase('video',['id' => $this->videoid, 'userid' => $this->userid, 'title' => $this->title, 'description' => $this->desc, 'topic' => $this->topic, 'course' => $this->course, 'thumbnail_path' => $this->thumbnail, 'video_path' => $this->video]);
+    
+        $video = new Video();
+        $videoinfo = $video->getVideo($this->videoid);
+
+        $this->assertEquals($videoinfo[0]['description'], $this->desc);
+        $this->assertEquals($videoinfo[0]['title'], $this->title);
+        $this->assertEquals($videoinfo[0]['thumbnail_path'], $this->thumbnail);
     }
 
     public function testGetVideoLecturer()
