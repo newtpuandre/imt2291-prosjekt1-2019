3dda30b55ddcc697ba7b2b8c031044a7e1960000
elisawaa@stud.ntnu.no <elisawbye@gmail.com>
2019-02-26 16:17:16 +0100

    Skeleton for comment, rating og video test

---
+++ tests/unit/CommentTest.php
@@ -0,0 +1,20 @@
+<?php 
+
+class CommentTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    
+}
---
+++ tests/unit/RatingTest.php
@@ -0,0 +1,20 @@
+<?php 
+
+class RatingTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    
+}
